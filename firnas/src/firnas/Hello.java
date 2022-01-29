8c93c0ceaef99ec965bc5c1f6992e93dda23e2d4
hit300b1 <hit300b1@gmail.com>
2022-01-29 10:41:56 +0530

    aa

---
+++ firnas/src/firnas/Hello.java
@@ -0,0 +1,5 @@
+package firnas;
+
+public class Hello {
+
+}
--- firnas/src/firnas/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package firnas;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("WELCOME TO JAVA....");
-	}
-}
