d480ec5b762560308acef43e3fb53f6f026a1436
hit300b1 <hit300b1@gmail.com>
2022-01-29 10:40:11 +0530

    second commit

---
+++ firnas/src/firnas/Calculator.java
@@ -0,0 +1,19 @@
+package firnas;
+
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Please enter a number...:");
+		int num1=scan.nextInt();//INPUT
+		
+		System.out.println("Please enter another number....:");
+		int num2=scan.nextInt();//INPUT
+		
+		int sum=num1+num2;//PROCESS
+		
+		System.out.println("Addition of two numbers is...:"+sum);//OUTPUT
+	}
+}
---
+++ firnas/src/firnas/HelloWorld.java
@@ -0,0 +1,7 @@
+package firnas;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO JAVA....");
+	}
+}
