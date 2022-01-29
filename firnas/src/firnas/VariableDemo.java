965d4cb58dd329d8b4ed3d2265a568ad3f458838
hit300b1 <hit300b1@gmail.com>
2022-01-29 12:24:43 +0530

    WE LEARNED VARIABLES TODAY - THANKYOU

---
+++ firnas/src/firnas/VariableDemo.java
@@ -0,0 +1,33 @@
+package firnas;
+
+public class VariableDemo {
+	public static void main(String[] args) {
+		//whole numbers
+		byte children=2;//8 bit - range - -128 to +127
+		short house=1200;//16 bit - range -32768 to +32767;
+		int age=47;//47 number is assigned to age variable of type int
+		long distanceToMoon=28320948340823089L;
+		
+		byte b=-128;
+		b=127;
+		
+		short s=-32768;
+		
+		//float is of size 32 bit
+		float weight=88.45f;//88.45 float value is assigned to weight variable of type float
+		weight=1220;
+		
+		//double is of size 64 bit
+		double d=34343.56;
+		
+		char initial='m';
+		char c=97;//ASCII - AMERICAN STANDARD CODE FOR INFORMATION INTERCHANGE
+		System.out.println(c);
+		
+		String aboutMe="I am a teacher who teach web 3.0 - First in the world..";
+		
+		boolean alive=true;
+		alive=false;
+		
+	}
+}
