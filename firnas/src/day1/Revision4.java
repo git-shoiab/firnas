ba8bb8449621b2a2b8220c65a369307b17390589
hit300b1 <hit300b1@gmail.com>
2022-04-09 11:41:50 +0530

    9-apr

---
+++ firnas/src/day1/Revision4.java
@@ -0,0 +1,26 @@
+package day1;
+
+public class Revision4 {
+	public static void main(String[] args) {
+		byte b1=10;
+		byte b2=20;
+		byte b3=(byte)(b1*b2);
+		System.out.println(b3);
+		System.out.println(b1*b2);
+		
+		int i=258;
+		byte b=(byte)i;
+		
+		System.out.println(b);
+		
+		float f=10;
+		int n=(int)f;
+		
+		f=n;
+		
+		char c=97;
+		n=c;
+		c=(char)n;
+		
+	}
+}
